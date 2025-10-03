7d0df8e38f3cff2d45c547fcdad871dd9b7928bc
Katelyn Campbell <kcgarnet121@gmail.com>
2025-10-02 20:34:33 -0400

    removing icons, & changing text

--- app/index.jsx
+++ app/index.jsx
@@ -3,12 +3,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text>This is the begining of Digital STEADI!</Text>
     </View>
   )
 }
 
 export default Home
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,                  
+    justifyContent: 'center', 
+    alignItems: 'center',
+    }    
+})
