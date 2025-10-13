aae6314e9cede7166f0ff69b009e61b846e184da
DenimBeans <thegooseman16@gmail.com>
2025-10-12 21:46:23 -0400

    Jean's attempt at the Home Page

--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,71 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, ImageBackground} from 'react-native'
+import { Colors } from '../constants/Colors' 
+import Shield from '../assets/Shield.png'
+import STDBtn from '../components/STDBtn'
 import React from 'react'
 
-const Home = () => {
+const index = () => 
+{
+
+  const tempSubmit = () =>
+  {
+    console.log("button pressed")
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>This is the begining of Digital STEADI!</Text>
+    <View style = {styles.container}>
+      <ImageBackground source = {Shield} style = {styles.shield}>
+        <Text style = {styles.tempText}>STEADI</Text>
+      </ImageBackground>
+      
+      <STDBtn onPress = {tempSubmit} style = {styles.btn}>
+        <Text style = {styles.btnText}>Get Started</Text>
+      </STDBtn>
+
     </View>
   )
 }
 
-export default Home
+export default index
 
-const styles = StyleSheet.create({
-    container: {
-    flex: 1,                  
-    justifyContent: 'center', 
+const styles = StyleSheet.create(
+{
+  container:
+  {
     alignItems: 'center',
-    }    
+    backgroundColor: Colors.primary,
+    flex: 1,
+    //justifyContent: 'center'
+  },
+
+  btn:
+  {
+    justifyContent: 'center',
+    top: "73.55%"
+  },
+
+  btnText:
+  {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textColor: "#ffffff",
+  },
+
+  tempText:
+  {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 48,
+    textAlign: 'center',
+  },
+
+  shield: 
+  {
+    height: 217,
+    justifyContent: 'center',
+    top: '34.54%',
+    width: 218
+  }
 })
