c8fe92f1611d4882f0c567df341c9dd4834e5910
mayeuse <ma369059@ucf.edu>
2026-02-03 13:43:39 -0500

    blood pressure entry (still on godmode, string formatting)

---
+++ src/utils/dataEntry.ts
@@ -0,0 +1,18 @@
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
+import { db, auth } from './gcipAuth';
+
+async function getPID(): Promise<string> {
+    const docRef = doc(db, "Users-AppData", auth.currentUser!.uid);
+    return (await getDoc(docRef)).data()?.participantID;
+};
+
+export async function enterBP(standing: string, lying: string): Promise<void> {
+    const pid = await getPID();
+    await updateDoc(doc(db, "Users-StudyData", pid), {
+        bloodPressure: {
+            standingBP: standing,
+            lyingBP: lying
+        }
+    });
+    return;
+};
