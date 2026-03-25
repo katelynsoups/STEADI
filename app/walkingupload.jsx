ebaccc54516c23f96cee04b1db64179f0e49135d
DenimBeans <thegooseman16@gmail.com>
2026-03-24 23:05:46 -0400

    Changed line to allow walking test video to play

--- app/walkingupload.jsx
+++ app/walkingupload.jsx
@@ -1,13 +1,11 @@
 import Upload from '../src/components/Upload';
 
-const visionVid = require('../src/assets/STEADItestvid.mp4');
-
 const TimedUpAndGoUpload = () => {
     return (
         <Upload
             test = {'walking'}
             text = {'Watch the video tutorial for completing the Up & Go test.'}
-            vid = {visionVid}
+            screenId = {'walking-test'}
             route = {'/success?test=walking'}
         />
     );
